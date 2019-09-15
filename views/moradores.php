3ad8ae6586c9a4ae53df55106296baf731912aa1
karenina-barros <karenina_silva@hotmail.com>
2019-09-15 14:29:45 -0300

    Correção do layout moradores.php

--- views/moradores.php
+++ views/moradores.php
@@ -1,120 +1,116 @@
-<div class="row">
-    <div class="col-12">
-        <div class="container bg-white px-4">
-            <div class="row py-3 mb-1 border-bottom">
-                <div class="col-sm">
-                    <h5 class="card-title text-uppercase green"><i class="fas fa-users pr-2"></i>Moradores</h5>
-                </div>
-                <div class="col-sm text-right">
-                    <a href="" class="btn bg-orange btn-sm"><i class="fas fa-plus pr-2"></i>Cadastrar Morador</a>
-                </div>
-            </div>
-            <div class="row my-4">
-                <div class="col-12">
-                    <table class="table table-sm table-hover font-9 text-cinza text-center">
-                        <thead class="bg-blue text-white">
-                            <tr>
-                                <th scope="col">Id</th>
-                                <th scope="col">Nome</th>
-                                <th scope="col">Apartamento</th>
-                                <th scope="col">Ações</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Lucia da silva Barros</td>
-                                <td>Otto</td>
-                                <td>
-                                    <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
-                                    <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresEditar" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
-                                    <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>
-                                    <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
-                                    <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
-                                    <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>
-                                    <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
-                                    <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
-                                    <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>
-                                    <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
-                                    <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
-                                    <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>
-                                    <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
-                                    <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
-                                    <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>
-                                    <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
-                                    <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
-                                    <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>
-                                    <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
-                                    <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
-                                    <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>
-                                    <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
-                                    <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
-                                    <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>
-                                    <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
-                                    <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
-                                    <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+<div class="container bg-white px-4">
+    <div class="row py-3 border-bottom">
+        <div class="col-sm">
+            <h6 class="card-title text-uppercase green mt-2 mb-0"><i class="fas fa-list-alt pr-2 "></i>Listagem</h6>
+        </div>
+        <div class="col-sm text-right">
+            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresCadastrar" class="btn bg-orange btn-sm font-8"><i class="fas fa-plus pr-2"></i>Cadastrar Morador</a>
+        </div>
+    </div>
+    <div class="row my-4">
+        <div class="col-12">
+            <table class="table table-sm table-hover font-9 text-cinza text-center">
+                <thead class="bg-blue text-white">
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Apartamento</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Lucia da silva Barros</td>
+                        <td>Otto</td>
+                        <td>
+                            <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
+                            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresEditar" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
+                            <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>
+                            <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
+                            <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
+                            <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>
+                            <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
+                            <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
+                            <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>
+                            <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
+                            <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
+                            <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>
+                            <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
+                            <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
+                            <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>
+                            <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
+                            <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
+                            <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>
+                            <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
+                            <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
+                            <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>
+                            <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
+                            <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
+                            <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>
+                            <a href="" class="btn btn-sm text-white btn-info mr-1"><i class="fas fa-eye pr-2"></i>Ver</a>
+                            <a href="" class="btn btn-sm text-white bg-yellow mr-1"><i class="fas fa-edit pr-2"></i>Editar</a>
+                            <a href="" class="btn btn-sm text-white bg-red"><i class="fas fa-trash-alt pr-2"></i>Remover</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
