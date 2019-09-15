1be452498ae266915979d0e8578fed6a5e451a6c
karenina-barros <karenina_silva@hotmail.com>
2019-09-15 17:20:58 -0300

    Alteração nos botões da tabela na view moradores.php

--- views/moradores.php
+++ views/moradores.php
@@ -1,7 +1,7 @@
 <div class="container bg-white px-4">
     <div class="row py-3 border-bottom">
         <div class="col-sm">
-            <h6 class="card-title green mt-2 mb-0"><i class="fas fa-list-alt pr-2 "></i>Listagem</h6>
+            <h6 class="card-title    green mt-2 mb-0"><i class="fas fa-list-alt pr-2 "></i>Listagem</h6>
         </div>
         <div class="col-sm text-right">
             <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresCadastrar" class="btn bg-orange btn-sm font-8"><i class="fas fa-plus pr-2"></i>Cadastrar Morador</a>
@@ -19,150 +19,24 @@
                     </tr>
                 </thead>
                 <tbody>
+                <?php for($i = 1; $i <= 12; $i++){ ?>
                     <tr>
-                        <th scope="row">1</th>
+                        <th scope="row"><?= $i ?></th>
                         <td>Lucia da silva Barros</td>
                         <td>Otto</td>
                         <td>
-                            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresPerfil" class="btn btn-sm text-white btn-info mr-1">
+                            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresPerfil" class="btn btn-sm text-white btn-info px-2 py-0 mr-1">
                                 <i class="fas fa-eye pr-2"></i>Ver
                             </a>
-                            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresEditar" class="btn btn-sm text-white bg-yellow mr-1">
+                            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresEditar" class="btn btn-sm text-white bg-yellow px-2 py-0 mr-1">
                                 <i class="fas fa-edit pr-2"></i>Editar
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-red">
+                            </a> 
+                            <a href="" class="btn btn-sm text-white px-2 py-0 bg-red">
                                 <i class="fas fa-trash-alt pr-2"></i>Remover
                             </a>
                         </td>
                     </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>
-                            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresPerfil" class="btn btn-sm text-white btn-info mr-1">
-                                <i class="fas fa-eye pr-2"></i>Ver
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-yellow mr-1">
-                                <i class="fas fa-edit pr-2"></i>Editar
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-red">
-                                <i class="fas fa-trash-alt pr-2"></i>Remover
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>
-                            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresPerfil" class="btn btn-sm text-white btn-info mr-1">
-                                <i class="fas fa-eye pr-2"></i>Ver
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-yellow mr-1">
-                                <i class="fas fa-edit pr-2"></i>Editar
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-red">
-                                <i class="fas fa-trash-alt pr-2"></i>Remover
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>
-                            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresPerfil" class="btn btn-sm text-white btn-info mr-1">
-                                <i class="fas fa-eye pr-2"></i>Ver
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-yellow mr-1">
-                                <i class="fas fa-edit pr-2"></i>Editar
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-red">
-                                <i class="fas fa-trash-alt pr-2"></i>Remover
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>
-                            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresPerfil" class="btn btn-sm text-white btn-info mr-1">
-                                <i class="fas fa-eye pr-2"></i>Ver
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-yellow mr-1">
-                                <i class="fas fa-edit pr-2"></i>Editar
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-red">
-                                <i class="fas fa-trash-alt pr-2"></i>Remover
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>
-                            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresPerfil" class="btn btn-sm text-white btn-info mr-1">
-                                <i class="fas fa-eye pr-2"></i>Ver
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-yellow mr-1">
-                                <i class="fas fa-edit pr-2"></i>Editar
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-red">
-                                <i class="fas fa-trash-alt pr-2"></i>Remover
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>
-                            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresPerfil" class="btn btn-sm text-white btn-info mr-1">
-                                <i class="fas fa-eye pr-2"></i>Ver
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-yellow mr-1">
-                                <i class="fas fa-edit pr-2"></i>Editar
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-red">
-                                <i class="fas fa-trash-alt pr-2"></i>Remover
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>
-                            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresPerfil" class="btn btn-sm text-white btn-info mr-1">
-                                <i class="fas fa-eye pr-2"></i>Ver
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-yellow mr-1">
-                                <i class="fas fa-edit pr-2"></i>Editar
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-red">
-                                <i class="fas fa-trash-alt pr-2"></i>Remover
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>
-                            <a href="http://localhost/Projetos/projeto-condominio/index.php?page=moradoresPerfil" class="btn btn-sm text-white btn-info mr-1">
-                                <i class="fas fa-eye pr-2"></i>Ver
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-yellow mr-1">
-                                <i class="fas fa-edit pr-2"></i>Editar
-                            </a>
-                            <a href="" class="btn btn-sm text-white bg-red">
-                                <i class="fas fa-trash-alt pr-2"></i>Remover
-                            </a>
-                        </td>
-                    </tr>
+                <?php } ?>
                 </tbody>
             </table>
         </div>
