9b2e3c8194c68a42a1561514222f211b36cf6f21
karenina-barros <karenina_silva@hotmail.com>
2019-09-19 22:34:28 -0300

    Criação do editor de texto na view novoComunicado.php

--- views/novoComunicado.php
+++ views/novoComunicado.php
@@ -6,15 +6,15 @@
     </div>
     <div class="row font-9 py-3">
         <div class="col-12">
-            <form class="text-cinza" action="http://localhost/Projetos/projeto-condominio/index.php?page=novoComunicado">
+            <form class="text-cinza" action="http://localhost/Projetos/projeto-condominio/index.php?page=novoComunicado" method="post">
                 <div class="form-row">
                     <div class="form-group col-md-4">
                         <label class="mb-0" for="inputAssunto">Assunto</label>
                         <input type="text" class="form-control form-control-sm" id="inputAssunto" placeholder="Digite o Assunto">
                     </div>
-                    <div class="form-group col-md-4">
+                    <div class="form-group col-md-12">
                         <label class="mb-0" for="inputDescricao">Descrição</label>
-                        <input type="text" class="form-control form-control-sm" id="inputDescricao" placeholder="Digite a Descrição">
+                        <textarea id="textComunicado" placeholder="Digite a Descrição"></textarea>
                     </div>
                 </div>
                 <button type="submit" class="btn btn-primary btn-sm px-4">Cadastrar</button>
@@ -22,3 +22,10 @@
         </div>
     </div>
 </div>
+
+<script src="vendor/tinymce/js/tinymce/tinymce.min.js"></script> 
+<script>
+    tinymce.init({
+        selector: '#textComunicado'
+    });
+</script>
